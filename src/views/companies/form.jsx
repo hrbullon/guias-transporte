33007a90d98a75c3cc00d8929120e3acaaf20f70
Haderson Bullon <hrbullon@gmail.com>
2021-06-10 02:10:16 -0400

    Agregar campos de responsables, municipio y parroquias

--- src/views/companies/form.jsx
+++ src/views/companies/form.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react'
 import { useForm } from "react-hook-form"
+import Select from 'react-select'
+
+import data from '../../data/municipios.json'
+import { prepareOptionsSelect } from '../../helpers/dataArray'
 
 export const Form = (props) => {
-
+    
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
+    let parroquias = []
+
+    let municipioItems = data.map(item => item.municipio);
+    const municipios = prepareOptionsSelect(municipioItems)
+
     useEffect(() => {
+
         reset({...props.data})
     }, [props.data])
 
@@ -26,11 +36,27 @@ export const Form = (props) => {
                 <input type="text" name="rif" autoComplete="off" className="form-control" {...register("rif", { required: true } )} placeholder="Ingrese el RIF/Cédula"/>
                 {errors.rif?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
             </div>
-            <div className="form-group mb-5">
-                <label className="control-label">Direccón *</label>
+            <div className="form-group">
+                <label className="control-label">Municipio *</label>
+                <Select name="municipio" isClearable={true} options={municipios} />
+            </div>
+            <div className="form-group">
+                <label className="control-label">Parroquia</label>
+                <Select name="parroquia" isClearable={true} options={parroquias} />
+            </div>
+            <div className="form-group">
+                <label className="control-label">Dirección *</label>
                 <input type="text" name="direccion" autoComplete="off" className="form-control" {...register("direccion", { required: true } )} placeholder="Ingrese la dirección"/>
                 {errors.direccion?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
             </div>
+            <div className="form-group mb-5">
+                <label className="control-label">Estado</label>
+                <select className="form-control" {...register("estado", { required: true } )}>
+                    <option value="Activo">Activo</option>
+                    <option value="Inactivo">Inactivo</option>
+                </select>
+            </div>
+
             { /**** Datos del representante legal/encargado de la empresa****/}
             <h4 className="card-title">Datos Representante</h4>
             <hr/>
@@ -49,13 +75,38 @@ export const Form = (props) => {
                 <input type="text" name="representante_telefono" autoComplete="off" className="form-control" {...register("representante_telefono", { required: true } )} placeholder="Ingrese la dirección"/>
                 {errors.representante_telefono?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
             </div>
-            <div className="form-group">
-                <label className="control-label">Estado</label>
-                <select className="form-control" {...register("estado", { required: true } )}>
-                    <option value="Activo">Activo</option>
-                    <option value="Inactivo">Inactivo</option>
-                </select>
-            </div>
+            { props.title == "Empresa" &&
+
+                <>
+                    <h4 className="card-title mt-5">Datos de Responsable</h4>
+                    <hr/>
+                    <div className="form-group">
+                        <label className="control-label">Nombre / Razón Social *</label>
+                        <input type="text" name="responsable_nombre" autoComplete="off" className="form-control" {...register("representante_nombre", { required: true } )} placeholder="Ingrese Nombre Representante"/>
+                        {errors.responsable_nombre?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">RIF/Cédula *</label>
+                        <input type="text" name="responsable_rif" autoComplete="off" className="form-control" {...register("representante_rif", { required: true } )} placeholder="Ingrese RIF/Cédula"/>
+                        {errors.responsable_rif?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Teléfono *</label>
+                        <input type="text" name="responsable_telefono" autoComplete="off" className="form-control" {...register("representante_telefono", { required: true } )} placeholder="Ingrese la dirección"/>
+                        {errors.responsable_telefono?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
+                    </div>
+
+                    <h4 className="card-title mt-5">Límite de vehículos por jornada</h4>
+                    <hr/>
+                    <div className="form-group">
+                        <label className="control-label">Cantidad de vehículos *</label>
+                        <input type="number" name="limite_vehiculos" autoComplete="off" className="form-control" {...register("limite_vehiculos", { required: true } )} placeholder="Ingrese la cantidad de vehículos"/>
+                        {errors.limite_vehiculos?.type === 'required' && <span className="text-danger">Este campo es obligatorio</span>}
+                    </div>
+
+                </>
+
+            }
 
             <button type="button" className="btn btn-inverse pull-right">Cancelar</button>
             <button type="submit" className="btn btn-success pull-right mr-2"> <i className="fa fa-check"></i> Guardar</button>
