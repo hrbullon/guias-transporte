c757986a425dc4b5d1f2fd14acaff4638e72f0bf
Haderson Bullon <hrbullon@gmail.com>
2021-10-03 22:56:27 -0400

    Logo en eel centro de la pantalla

--- src/views/starter/starter.jsx
+++ src/views/starter/starter.jsx
@@ -14,11 +14,12 @@ import {
 import img1 from '../../assets/images/big/img1.jpg';
 import img2 from '../../assets/images/big/img2.jpg';
 import img3 from '../../assets/images/big/img3.jpg';
+import logo from '../../assets/images/logo.png';
 
 const Starter = () => {
     return (
-        <div>
-            teasd
+        <div className="text-center">
+            <img src={logo} alt="" width="300"/>
         </div>
     );
 }
